3acbe8a07c6e18b85bb219d0d446d0f0e5574f5d
PierreTchuente <33568610+PierreTchuente@users.noreply.github.com>
2018-03-09 23:40:52 +0200

    adding current user location and webpack confing file
    adding current user location and webpack config file

--- src/index.js
+++ src/index.js
@@ -1,53 +1,65 @@
 import _ from 'angular';
 
-var initialiseMap = function() {
-
-    //Default Position.
-    var defaultPosition = {
-        lat: '-2.39794',
-        lng: '27.6944'
-    };
-    var map = new google.maps.Map(document.getElementById('mapID'), {
-        zoom: 4,
-        center: defaultPosition
-    });
-    var marker = new google.maps.Marker({
-        position: defaultPosition,
-        map: map
-    });
-
-    var inputSeachElem = document.getElementById('inputSeachID');
-    var autocomplete = new google.maps.places.Autocomplete(inputSeachElem);
-
-    autocomplete.addListener('place_changed', function() {
-
-        marker.setVisible(false);
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            // if the place has not been sugested.
-            window.alert("No details available for input: '" + place.name + "'");
-            alert("Sorry!! could not find " + place.nane);
-            return false;
-        }
-
-        console.log(place);
-
-        //if we have a geometric position, present on the map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);
-        }
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-    });
-}
-
-window.initialiseMap = initialiseMap;
-
-// Actually, I haven't use angular for this exercise.
 var mainMod = angular.module('mainApp', []);
 mainMod.controller('mainController', ['$scope', function($scope) {
 
+    var initialiseMap = function() {
+
+        //Default Position.
+        var defaultPosition = {
+            lat: -26.39794,
+            lng: 27.6944
+        };
+        var map = new google.maps.Map(document.getElementById('mapID'), {
+            zoom: 17,
+            center: defaultPosition
+        });
+        var marker = new google.maps.Marker({
+            position: defaultPosition,
+            map: map
+        });
+
+        if (navigator.geolocation) {
+            //Starting from the user current location.
+            navigator.geolocation.getCurrentPosition(function(position) {
+                defaultPosition.lat = position.coords.latitude;
+                defaultPosition.lng = position.coords.longitude;
+
+                console.log(defaultPosition);
+
+                map.setCenter(defaultPosition);
+            });
+        } else {
+            console.log("Browser does not support Geolocation.");
+        }
+
+        var inputSeachElem = document.getElementById('inputSeachID');
+        var autocomplete = new google.maps.places.Autocomplete(inputSeachElem);
+
+        autocomplete.addListener('place_changed', function() {
+
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                // if the place has not been sugested.
+                window.alert("No details available for input: '" + place.name + "'");
+                alert("Sorry!! could not find " + place.nane);
+                return false;
+            }
+
+            console.log(place);
+
+            //if we have a geometriy position, present on the map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);
+            }
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+        });
+    }
+
+    initialiseMap(); // initialise the map.
 }]);
